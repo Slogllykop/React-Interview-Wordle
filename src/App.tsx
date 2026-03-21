e284bf463321516a0334be9b3cacf8faae578c32
Slogllykop <slogllykop07@gmail.com>
2026-03-21 17:14:16 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <h2 className="px-1 text-xl text-yellow-500">hello</h2>;
+}
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const WORDLE_WORDS_URL =
+    "https://api.frontendexpert.io/api/fe/wordle-words";
