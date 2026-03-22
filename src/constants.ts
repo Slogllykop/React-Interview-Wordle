d18a225692a6f258f2ce3aa198727902612b13c3
Slogllykop <slogllykop07@gmail.com>
2026-03-22 23:59:42 +0530

    Fix CORS by adding proxy api

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
+// export const WORDLE_WORDS_URL =
+//     "https://api.frontendexpert.io/api/fe/wordle-words";
 export const WORDLE_WORDS_URL =
-    "https://api.frontendexpert.io/api/fe/wordle-words";
+    "https://mocki.io/v1/08f7964c-900a-4f9d-8baf-d8ecb7b8b977";
